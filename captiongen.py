4ff4e198bab032a8567d3bca03e3e13c053dcff4
Trupti Gavit <163740738+TruptiGavit@users.noreply.github.com>
2024-04-02 01:07:46 +0530

    Update captiongen.py

--- captiongen.py
+++ captiongen.py
@@ -1,7 +1,7 @@
 import requests
 
 API_URL = "https://api-inference.huggingface.co/models/mistralai/Mixtral-8x7B-Instruct-v0.1"
-headers = {"Authorization": "Bearer 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"}
+headers = {"Authorization": "My_HuggingFaceHub_Key"}
 
 def query(payload):
 	response = requests.post(API_URL, headers=headers, json=payload)
